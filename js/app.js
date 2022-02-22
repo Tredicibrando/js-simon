0e43e5ccdd1537b6c87d58209a6b2be1a3183a5f
Tredicibrando <nicolosigiov@gmail.com>
2022-02-22 15:36:18 +0100

    logica gioco

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
-console.log(ciao);
+console.log('ciao');
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let n1 = getRandomIntInclusive(0, 50);
+    n2 = getRandomIntInclusive(0, 50);
+    n3 = getRandomIntInclusive(0, 50);
+    n4 = getRandomIntInclusive(0, 50);
+    n5 = getRandomIntInclusive(0, 50);
+
+const numeri = [n1, n2, n3, n4, n5];
+console.log(numeri);
+
+alert(`
+         ATTENZIONE!
+    memorizza questi numeri:
+         ${numeri}
+`);
+
+let activeIndex = 0;
+
+numeri.forEach(element => {
+    let numeroGiocatore = parseInt(prompt(''));
+    console.log(numeroGiocatore)
+
+    if(numeroGiocatore !== element){
+        console.log('mi dispiace la combinazione è errata')
+    } else {
+        console.log(`congratulazioni! i numeri sono ${numeroGiocatore},${element}`)
+        activeIndex++;
+    }
+ });
+
+ console.log(activeIndex)
+
+
+
+
+
+
+
